3daa5266768da8c9ad2246b77fadfe52c00b2cee
azu <azuciao@gmail.com>
2022-07-30 17:37:24 +0900

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+## Usage
+
+    npm install
+    npm run build
+
+Throw an error
+
+>  Property id of VariableDeclarator expected node to be of a type ["LVal"] but instead got "CallExpression"
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import * as path from "path";
+import * as url from "url";
+
+const __filename__ = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename__);
+console.log(__dirname);
